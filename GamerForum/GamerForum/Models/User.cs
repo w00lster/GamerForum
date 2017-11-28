0a672f70d228191c73f0fb59477d817b36ae0908
Morten Rahbæk Uldum <eaamru@students.eaaa.dk>
2017-11-28 11:12:17 +0100

    Commit phone

--- GamerForum/GamerForum/Models/User.cs
+++ GamerForum/GamerForum/Models/User.cs
@@ -12,5 +12,6 @@ namespace GamerForum.Models
         public string LastName { get; set; }
         public string Email { get; set; }
         public string MidName { get; set; }
+        public string PhoneNumber { get; set; }
     }
 }
