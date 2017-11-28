594ba8f46512c5e655dc8d020d12bf9cd56c9010
Morten Rahbæk Uldum <eaamru@students.eaaa.dk>
2017-11-28 11:03:01 +0100

    Email Commit

--- GamerForum/GamerForum/Models/User.cs
+++ GamerForum/GamerForum/Models/User.cs
@@ -10,5 +10,6 @@ namespace GamerForum.Models
         public string UserName { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string Email { get; set; }
     }
 }
