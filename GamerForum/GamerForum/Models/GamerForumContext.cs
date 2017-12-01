060c6a2b762892ad3a6daeae4dfb7a35675f39af
Morten Rahbæk Uldum <eaamru@students.eaaa.dk>
2017-12-01 12:09:55 +0100

    Entity commit

--- GamerForum/GamerForum/Models/GamerForumContext.cs
+++ GamerForum/GamerForum/Models/GamerForumContext.cs
@@ -1,13 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-
 using System.Web;
+using System.Data.Entity;
+
 
 namespace GamerForum.Models
 {
-    public class GamerForumContext
-
+    public class GamerForumContext: DbContext
     {
     }
 }
