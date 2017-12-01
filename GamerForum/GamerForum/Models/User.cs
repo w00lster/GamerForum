8bdc1e70f826991411015229bdc9597c54a039a6
Skjold Reimers Larsen <eaasrl@students.eaaa.dk>
2017-12-01 10:06:21 +0100

    skjold commit

--- GamerForum/GamerForum/Models/User.cs
+++ GamerForum/GamerForum/Models/User.cs
@@ -6,12 +6,16 @@ using System.Web;
 namespace GamerForum.Models
 {
     public class User{
-        public int Age { get; set; }
-        public string UserName { get; set; }
+        public int user_id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
         public string Email { get; set; }
-        public string MidName { get; set; }
-        public string PhoneNumber { get; set; }
+        public string image { get; set; }
+        public int age { get; set; }
+        public DateTime day_created { get; set; }
+        
+   
     }
 }
