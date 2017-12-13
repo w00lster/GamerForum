de6174a2cfaa730a7e060052870d73ee4c6c98cc
Morten Rahbæk Uldum <eaamru@students.eaaa.dk>
2017-12-13 17:45:48 +0100

    alt kristines, plus lidt fra mig commit

--- GamerForum/GamerForum/Content/Js/Scripts.js
+++ GamerForum/GamerForum/Content/Js/Scripts.js
@@ -1,10 +1,16 @@
-﻿if (document.getElementById("front_page_wrapper")) {
+﻿if (document.getElementById("front-page")) {
     document.getElementById("general-search").style.display = "none";
+    document.getElementById("navigation-circles").style.display = "none";
+    
 }
 
-if (document.getElementById("front_page_wrapper")) {
+if (document.getElementById("front-page")) {
     document.getElementById("body-wrapper").style.backgroundImage = "url(../../Content/Image/background.jpeg)";
 } else if (document.getElementById("wanted-games-create")) {
     document.getElementById("body-wrapper").style.backgroundPosition = "top center";
+    document.getElementById("body-wrapper").style.backgroundImage = "url(../../Content/Image/boardgameBackground1.jpg)";
+} else if (document.getElementById("user-create")) {
+    document.getElementById("body-wrapper").style.backgroundImage = "url(../../Content/Image/boardgameBackground1.jpg)";
+} else if (document.getElementById("user-profile")) {
     document.getElementById("body-wrapper").style.backgroundImage = "url(../../Content/Image/boardgameBackground1.jpg)";
 }
