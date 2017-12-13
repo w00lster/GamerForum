317bbaa244b6e50ad7cd3486db7a8419a2478a69
Kristine Dupont Johannessen <eaakdj@students.eaaa.dk>
2017-12-13 11:15:44 +0100

    Baggrund og søgefelt i top styles

--- GamerForum/GamerForum/Content/Js/Scripts.js
+++ GamerForum/GamerForum/Content/Js/Scripts.js
@@ -1,3 +1,10 @@
 ﻿if (document.getElementById("front_page_wrapper")) {
     document.getElementById("general-search").style.display = "none";
 }
+
+if (document.getElementById("front_page_wrapper")) {
+    document.getElementById("body-wrapper").style.backgroundImage = "url(../../Content/Image/background.jpeg)";
+} else if (document.getElementById("games")) {
+    document.getElementById("body-wrapper").style.backgroundPosition = "top center";
+    document.getElementById("body-wrapper").style.backgroundImage = "url(../../Content/Image/background1.jpg)";
+}
