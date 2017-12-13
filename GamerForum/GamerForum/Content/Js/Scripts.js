110bd57750685e2bfd16fc25c0e999bbbf7dbe6f
Kristine Dupont Johannessen <eaakdj@students.eaaa.dk>
2017-12-13 22:13:39 +0100

    Layout changes commit

--- GamerForum/GamerForum/Content/Js/Scripts.js
+++ GamerForum/GamerForum/Content/Js/Scripts.js
@@ -5,12 +5,17 @@
 }
 
 if (document.getElementById("front-page")) {
-    document.getElementById("body-wrapper").style.backgroundImage = "url(../../Content/Image/background.jpeg)";
+    document.getElementById("body-wrapper").style.backgroundImage = "url(../../Content/Image/frontpage.jpg)";
+    document.getElementById("body-wrapper").style.backgroundPosition = "center center";
 } else if (document.getElementById("wanted-games-create")) {
     document.getElementById("body-wrapper").style.backgroundPosition = "top center";
     document.getElementById("body-wrapper").style.backgroundImage = "url(../../Content/Image/boardgameBackground1.jpg)";
 } else if (document.getElementById("user-create")) {
-    document.getElementById("body-wrapper").style.backgroundImage = "url(../../Content/Image/boardgameBackground1.jpg)";
+    document.getElementById("body-wrapper").style.backgroundImage = "url(../../Content/Image/usercreate_new.jpg)";
 } else if (document.getElementById("user-profile")) {
-    document.getElementById("body-wrapper").style.backgroundImage = "url(../../Content/Image/boardgameBackground1.jpg)";
+    document.getElementById("body-wrapper").style.backgroundImage = "url(../../Content/Image/usercreate_new.jpg)";
+} else if (document.getElementById("games")) {
+    document.getElementById("body-wrapper").style.backgroundImage = "url(../../Content/Image/boardgameBackground3.jpg)";
+} else if (document.getElementById("create-games")) {
+    document.getElementById("body-wrapper").style.backgroundImage = "url(../../Content/Image/boardgameBackground3.jpg)";
 }
